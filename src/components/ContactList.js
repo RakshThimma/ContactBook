7e956029788e235c1b516c02c1c606c84d3680c2
RakshThimma <rakshithathimma@gmail.com>
2025-07-02 16:25:36 +0530

    delete prompt and copy clipboard

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,10 +5,14 @@ import { Link } from "react-router-dom";
 const ContactList = () => {
   const { contacts, setContacts } = useContext(ContactContext);
   console.log(contacts);
-  const handleDelete = (id)=> {
-       setContacts((prev) => prev.filter((c)=> c.id !== id ))
-
-  }
+  const handleDelete = (id) => {
+    let data = window.prompt(
+      "Are you sure you want to delete this contact? Type 'yes' to proceed."
+    );
+    if (data === "yes") {
+      setContacts((prev) => prev.filter((c) => c.id !== id));
+    }
+  };
   return (
     <div>
       <div className="shadow-md grid grid-cols-9 text-center bg-gray-300 p-4">
@@ -31,17 +35,43 @@ const ContactList = () => {
             <div>{contact.id}</div>
             <div className="text-center">{contact.fname}</div>
             <div className="text-center">{contact.lname}</div>
-            <div className="truncate max-w-[150px] overflow-hidden">
-              {contact.email}
+            <div className="flex items-center">
+              <div className="truncate max-w-[150px] overflow-hidden">
+                <a href={`mailto:${contact.email}`}>{contact.email}</a>
+              </div>
+              <div>
+                {contact.email && <img
+                  src="https://img.icons8.com/?size=100&id=86206&format=png&color=000000\"
+                  alt="copy"
+                  onClick={(e) => {
+                    navigator.clipboard.writeText(contact.email);
+                    // console.log(e.target.src)
+                    e.target.src =
+                      "https://img.icons8.com/?size=100&id=98955&format=png&color=000000";
+                    setTimeout(() => {
+                      e.target.src =
+                        "https://img.icons8.com/?size=100&id=86206&format=png&color=000000";
+                    }, 3000);
+                  }}
+                  className="w-5 cursor-pointer"
+                />}
+              </div>
             </div>
             <div>{contact.number}</div>
             <div className="truncate max-w-[150px] overflow-hidden">
-              {contact.linkdn}
+              <a href={contact.linkdn} target="_blank">{contact.linkdn}</a>*
             </div>
             <div>Active</div>
-            <Link to={`/edit?id=${contact.id}`}><div className="text-green-400 hover:text-green-700">Edit</div></Link>
+            <Link to={`/edit?id=${contact.id}`}>
+              <div className="text-green-400 hover:text-green-700">Edit</div>
+            </Link>
             <div>
-              <button onClick={()=>handleDelete(contact.id)} className="text-red-400 hover:text-red-700">Delete</button>
+              <button
+                onClick={() => handleDelete(contact.id)}
+                className="text-red-400 hover:text-red-700"
+              >
+                Delete
+              </button>
             </div>
           </div>
         );
